b38f28199c8cc4873b17428313d04171f47821a1
Lukasz Sreniawski <sreniok@gmail.com>
2021-11-10 22:14:46 +0000

    q1

---
+++ Question_2/q_2.py
@@ -0,0 +1,52 @@
+# ---------------------------------------------------------------------
+# 
+# File          : q_1.py  
+# Created       : 09/11/2021 16:14
+# Author       : Lukasz S.
+# Version       : v1.0.0
+# Licencing     : (C) 2021 Lukasz S.
+#
+# Description   :
+# ---------------------------------------------------------------------
+
+from bs4 import BeautifulSoup
+import requests
+
+def read_page_contents():
+    """ scrape web page contents """
+    print("Contents of Page")
+
+    html_doc = requests.get("http://192.168.45.130")
+    soup = BeautifulSoup(html_doc.text, 'html.parser')
+
+    """searching all headings by class : section_headers"""
+    headers = soup.find_all(class_= "section_header")
+    headers_count = len(headers)
+
+    print('There are: {} headings'.format(headers_count))
+
+    """count word Apache2"""
+    pattern = 'Apache2'
+    word = soup.find_all(text=lambda text: text and pattern in text)
+    words_count = len(word)
+
+    print("Word Apache2 appears {} times".format(words_count))
+
+
+    """ find what the title is"""
+    title = soup.title
+
+    print("The title of website is: {}".format(title.string))
+
+
+
+if __name__ == "__main__":
+     read_page_contents()
+
+
+
+
+
+
+
+
