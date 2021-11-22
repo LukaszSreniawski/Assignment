873bd0179ed772d6d64325fd13a731b3599f9ab7
Lukasz Sreniawski <sreniok@gmail.com>
2021-11-22 13:08:01 +0000

    changing name

--- Question_4/L00170171_Q4_File_2.py
+++ Question_4/L00170171_Q4_File_2.py
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------
 # 
-# File          : q_4.py  
+# File          : L00170171_Q4_File_2.py
 # Created       : 22/11/2021 10:28
 # Author        : Lukasz S.
 # Version       : v1.0.0
@@ -9,3 +9,32 @@
 # Description   :Determine which ports are open
 #
 # ---------------------------------------------------------------------
+import socket
+import re
+
+# You can scan 0-65535 ports.
+port_min = 0
+port_max = 90
+
+# Ask user to input the ip address they want to scan.
+while True:
+    ip = input("\nPlease enter the ip address that you want to scan: ")
+    break
+
+open_ports = []
+
+# Socket port scanning
+for port in range(port_min, port_max + 1):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((ip, port))
+            open_ports.append(port)
+    except:
+        pass
+
+# showing open ports
+for port in open_ports:
+    if port == 22:
+        print(f"SSH: port {port} is open on ip: {ip}.")
+    elif port == 80:
+        print(f"HTTP: port {port} is open on ip: {ip}.")
